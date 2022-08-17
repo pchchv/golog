f2bef84810c5c95d19fb3e2bb9f103989d095935
pchchv <ipchchv@gmail.com>
2022-08-17 10:17:36 +0300

    Implement a method that returns plain glg instance

--- src/golog.go
+++ src/golog.go
@@ -1,12 +1,15 @@
 package golog
 
 import (
+	"bytes"
 	"io"
 	"sync"
+	"sync/atomic"
 )
 
 // Golog is base struct
 type Golog struct {
+	bs          *uint64
 	logger      loggers
 	level       *uint32
 	buffer      sync.Pool
@@ -29,6 +32,9 @@ type wMode uint8
 
 type traceMode int64
 
+// LEVEL is log level
+type LEVEL uint8
+
 type logger struct {
 	tag              string
 	rawtag           []byte
@@ -41,4 +47,39 @@ type logger struct {
 	prevMode         MODE
 	writeMode        wMode
 	disableTimestamp bool
+}
+
+const (
+	// DEBG is debug log level
+	DEBG LEVEL = iota + 1
+	// FATAL is fatal log level
+	FATAL
+	timeFormat         = "2006-01-02 15:04:05"
+	tab                = "\t"
+	lsep               = tab + "["
+	lsepl              = len(lsep)
+	sep                = "]:" + tab
+	sepl               = len(sep)
+	DefaultCallerDepth = 2
+)
+
+// New returns plain glg instance
+func New() *Golog {
+	g := &Golog{
+		level:       new(uint32),
+		callerDepth: DefaultCallerDepth,
+	}
+	g.bs = new(uint64)
+
+	atomic.StoreUint64(g.bs, uint64(len(timeFormat)+lsepl+sepl))
+
+	g.buffer = sync.Pool{
+		New: func() interface{} {
+			return bytes.NewBuffer(make([]byte, 0, int(atomic.LoadUint64(g.bs))))
+		},
+	}
+
+	atomic.StoreUint32(g.level, uint32(FATAL))
+
+	return g
 }
