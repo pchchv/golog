10b48c4e213848c7dcb3dc64d54bd482332ad846
pchchv <ipchchv@gmail.com>
2022-08-17 10:06:48 +0300

    Add logger struct

--- src/golog.go
+++ src/golog.go
@@ -1,6 +1,9 @@
 package golog
 
-import "sync"
+import (
+	"io"
+	"sync"
+)
 
 // Golog is base struct
 type Golog struct {
@@ -8,4 +11,25 @@ type Golog struct {
 	level      uint32
 	buffer     sync.Pool
 	enableJSON bool
+}
+
+// MODE is logging mode (std only, writer only, std & writer)
+type MODE uint8
+
+type wMode uint8
+
+type traceMode int64
+
+type logger struct {
+	tag              string
+	rawtag           []byte
+	writer           io.Writer
+	std              io.Writer
+	color            func(string) string
+	isColor          bool
+	traceMode        traceMode
+	mode             MODE
+	prevMode         MODE
+	writeMode        wMode
+	disableTimestamp bool
 }
