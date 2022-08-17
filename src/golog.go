dfd7b75c8dec5ee6109644f2e9421e19c6dfa642
pchchv <ipchchv@gmail.com>
2022-08-17 10:13:44 +0300

    Add JSON format struct

--- src/golog.go
+++ src/golog.go
@@ -7,10 +7,19 @@ import (
 
 // Golog is base struct
 type Golog struct {
-	logger     loggers
-	level      uint32
-	buffer     sync.Pool
-	enableJSON bool
+	logger      loggers
+	level       *uint32
+	buffer      sync.Pool
+	callerDepth int
+	enableJSON  bool
+}
+
+// JSONFormat is json object structure for logging
+type JSONFormat struct {
+	Date   string      `json:"date,omitempty"`
+	Level  string      `json:"level,omitempty"`
+	File   string      `json:"file,omitempty"`
+	Detail interface{} `json:"detail,omitempty"`
 }
 
 // MODE is logging mode (std only, writer only, std & writer)
