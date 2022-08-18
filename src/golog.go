2884dfe2b0b3c3548e85e93169a7b3a453e4aa94
pchchv <ipchchv@gmail.com>
2022-08-18 12:43:05 +0300

    Implement file path addition

--- src/golog.go
+++ src/golog.go
@@ -1,8 +1,37 @@
 package golog
 
+import (
+	"errors"
+	"path/filepath"
+)
+
 // Golog is base struct
 type Golog struct {
 	l      logger
 	saving bool // If false doesn't write to the file
-	file   string
+	path   string
+}
+
+func New(saving bool, path string) (*Golog, error) {
+	var err error
+	g := &Golog{}
+	g.saving, g.path, err = enablingFile(saving, path)
+	if err != nil {
+		return g, err
+	}
+	return g, nil
+}
+
+func enablingFile(s bool, p string) (bool, string, error) {
+	e := errors.New("The file path was set incorrectly")
+	if s {
+		if p == "" {
+			return s, p, e
+		}
+		p, err := filepath.Abs(p)
+		if err != nil {
+			return s, p, e
+		}
+	}
+	return s, p, nil
 }
