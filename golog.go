2c2122c644bb78bd50cdd7b3aada858e3a4b8a25
pchchv <ipchchv@gmail.com>
2023-01-22 12:54:16 +0300

    Implement a print functions

--- golog.go
+++ golog.go
@@ -1,1 +1,14 @@
 package golog
+
+import (
+	"fmt"
+	"os"
+)
+
+func LogPlain(writer *os.File, time, level string, maxLength int, caller, message string) {
+	fmt.Fprintf(writer, "%s\n", message)
+}
+
+func LogDefault(writer *os.File, time, level string, maxLength int, caller, message string) {
+	fmt.Fprintf(writer, "%s %s %-*s | %s\n", time, level, maxLength, caller, message)
+}
