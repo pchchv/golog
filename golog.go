0666a16e8f81f3dff18991e3f924f027b995ba79
pchchv <ipchchv@gmail.com>
2023-01-22 13:26:48 +0300

    Implement a error

--- golog.go
+++ golog.go
@@ -89,6 +89,19 @@ func Debugb(framesBackward int, format string, a ...interface{}) {
 	}
 }
 
+func Error(format string, a ...interface{}) {
+	if LogLevel <= LOG_ERROR {
+		log(LOG_ERROR, 3, fmt.Sprintf(format, a...))
+	}
+}
+
+// Errorb is equal to Error(...) but can go back in the stack and can therefore show function positions from previous functions.
+func Errorb(framesBackward int, format string, a ...interface{}) {
+	if LogLevel <= LOG_ERROR {
+		log(LOG_ERROR, 3+framesBackward, fmt.Sprintf(format, a...))
+	}
+}
+
 func log(level Level, framesBackward int, message string) {
 	// We know here that the stack contains two calls from inside this file.
 	// The third frame comes from the file that initially called a function
