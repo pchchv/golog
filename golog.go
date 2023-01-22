0b450a2021cf20afc0da450e1670d72385d54cc5
pchchv <ipchchv@gmail.com>
2023-01-22 12:56:55 +0300

    Add a config variables

--- golog.go
+++ golog.go
@@ -5,6 +5,48 @@ import (
 	"os"
 )
 
+type Level int
+
+const (
+	LOG_PLAIN Level = iota
+	LOG_DEBUG
+	LOG_INFO
+	LOG_ERROR
+	LOG_FATAL
+)
+
+var (
+	LogLevel   = LOG_INFO
+	DateFormat = "2006-01-02 15:04:05.000"
+
+	FormatFunctions = map[Level]func(*os.File, string, string, int, string, string){
+		LOG_PLAIN: LogPlain,
+		LOG_DEBUG: LogDefault,
+		LOG_INFO:  LogDefault,
+		LOG_ERROR: LogDefault,
+		LOG_FATAL: LogDefault,
+	}
+
+	// The current maximum length printed for caller information. This is updated each time something gets printed
+	CallerColumnWidth = 0
+
+	LevelStrings = map[Level]string{
+		LOG_PLAIN: "",
+		LOG_DEBUG: "[DEBUG]",
+		LOG_INFO:  "[INFO] ",
+		LOG_ERROR: "[ERROR]",
+		LOG_FATAL: "[FATAL]",
+	}
+
+	LevelOutputs = map[Level]*os.File{
+		LOG_PLAIN: os.Stdout,
+		LOG_DEBUG: os.Stdout,
+		LOG_INFO:  os.Stdout,
+		LOG_ERROR: os.Stderr,
+		LOG_FATAL: os.Stderr,
+	}
+)
+
 func LogPlain(writer *os.File, time, level string, maxLength int, caller, message string) {
 	fmt.Fprintf(writer, "%s\n", message)
 }
